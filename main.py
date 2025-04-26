e96c7213f0863486dcaa4ddbab5cffd001a2e4d0
Nikad023 <brunopiresalmeida94190@gmail.com>
2025-04-26 21:37:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from threading import Thread
+from keep_alive import keep_alive
 import discord
 from discord.ext import commands, tasks
 import random
@@ -150,17 +149,5 @@ async def commandes(ctx):
 """)
 
 import os
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot en ligne !"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-    keep_alive()
+keep_alive()
 bot.run(os.getenv('DISCORD_TOKEN'))
